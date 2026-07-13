bc9523be58320c19426bb5e9b8ea20e8b9a0f406
assadshahbaz <shehbaz.assad03@gmail.com>
2026-07-13 16:06:29 +0100

    chore: added route layer for prerender routes #5

--- web/src/app/app.config.server.ts
+++ web/src/app/app.config.server.ts
@@ -1,12 +1,15 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { API_URL } from './tokens/api-url.token';
 import { environment } from '../environments/environment';
+import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideServerRouting(serverRoutes),
     { provide: API_URL, useValue: process.env['API_INTERNAL_URL'] || environment.apiUrl },
   ]
 };
